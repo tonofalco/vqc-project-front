3b6f69ce362efb3ba872142f9bc7b4be78eec2ff
Antonio <tonofalcon22@gmail.com>
2025-09-14 21:58:29 -0600

    feat(tables): Implementacion de tablas en dashboard

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,13 @@
 
 export * from './jira/JiraTasks';
 
-
 export * from './shared/sidemenu/SideMenu';
 export * from './shared/cards/WhiteCard';
 export * from './shared/cards/WhiteCard';
+export * from './shared/tables/MyTravelsTable'
+export * from './shared/tables/TravelTable';
 
+
+export * from './dashboard/GeneralDashboard';
+export * from './dashboard/LandTravelsDashboard';
+export * from './dashboard/AirTravelsDashboard';
---
+++ src/components/shared/tables/MyTravelsTable.tsx
@@ -0,0 +1,111 @@
+import { Card, Typography } from "@material-tailwind/react";
+import { formatDate, filterPastEvents  } from "../../../helpers";
+
+const TABLE_HEAD = ["Fecha salida", "Cliente", "Destino", ""];
+
+interface TravelTableProps {
+  currentPage: number;
+  itemsPerPage: number;
+  setPage: (page: number) => void;
+  events: Array<any>;
+}
+
+export const MyTravelsTable = ({currentPage, itemsPerPage, setPage, events}: TravelTableProps) => {
+
+  // filtrar usando el campo "start"
+  const pastEvents = filterPastEvents (events, "start");
+
+  // calcular rango de eventos para la página actual
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedEvents = pastEvents.slice(startIndex, endIndex);
+
+  const tdClasses = "p-4 border-b border-blue-gray-50 max-w-[250px] break-words";
+
+
+  return (
+    <Card className="h-full w-full overflow-auto" {...({} as any)}>
+      <table className="w-full min-w-max table-auto text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th
+                key={head}
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+              >
+                <Typography {...({} as any)}
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {events && events.length > 0 ? (
+            paginatedEvents.map(({ id, start, nameClient, destination }, index) => {
+              
+              const isLast = index === events.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50";
+
+              return (
+                <tr key={id}>
+                  <td className={`${classes} ${tdClasses}`}>
+                    <Typography {...({} as any)}
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {formatDate(start)}
+                    </Typography>
+                  </td>
+                  <td className={`${classes} ${tdClasses}`}>
+                    <Typography {...({} as any)}
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {nameClient}
+                    </Typography>
+                  </td>
+                  <td className={`${classes} ${tdClasses}`}>
+                    <Typography {...({} as any)}
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {destination}
+                    </Typography>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan={TABLE_HEAD.length} className="p-4 text-center">
+                No hay viajes registrados proximos
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      <div className="flex justify-start lg:justify-center gap-2 mt-4">
+        {Array.from({ length: Math.ceil(pastEvents.length / itemsPerPage) }, (_, i) => (
+          <button
+            key={i}
+            className={`px-3 py-1 rounded ${currentPage === i + 1 ? "bg-blue-500 text-white" : "bg-gray-200"}`}
+            onClick={() => setPage(i + 1)}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
+    </Card>
+  );
+};
